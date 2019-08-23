9218b40bc39c5ac75f3c803351a928c1e7520e14
luoxingze <luoxiongze@koolearn-inc.com>
2019-08-23 18:27:51 +0800

    新增发言模板

--- src/index.js
+++ src/index.js
@@ -10,10 +10,13 @@ const symbols = require('log-symbols');
 const util = require('./util/util');
 
 program.version('1.0.0', '-v, --version')
-       .command('init <arg>')
-       .action( (arg) => {
-            let templates = fs.readdirSync('./template');
-            console.log(templates);
+    //    .command('init <arg>')
+       .command('init')
+       .action( () => {
+            let records = fs.readdirSync('./record');
+            let speechs = fs.readdirSync('./speech');
+            console.log(records);
+            console.log(speechs);
             
             inquirer.prompt([
                 {
@@ -22,8 +25,12 @@ program.version('1.0.0', '-v, --version')
                     message: '请输入人数'
                 },{
                     type: 'input',
-                    name: 'templateIndex',
-                    message: `请输入想使用的模板的序号，可选模板：\n${templates.map((item,index) => `${index}.${chalk.green(item)}`).join('\n')}\n`
+                    name: 'recordIndex',
+                    message: `请输入想使用的模板的序号，可选模板：\n${records.map((item,index) => `${index}.${chalk.green(item)}`).join('\n')}\n`
+                },{
+                    type: 'input',
+                    name: 'roleIndex',
+                    message: `请输入发言模板(输入身份缩写或者序号)，可选：\n${speechs.map((item,index) => `${index}.${chalk.green(item)}`).join('\n')}\n`
                 }
             ]).then((answers) => {
                 // console.log(answers);
@@ -36,13 +43,33 @@ program.version('1.0.0', '-v, --version')
                     const meta = Object.assign({}, answers);
                     console.log(symbols.success, chalk.green( '你的配置是：'));
                     console.log(chalk.green(JSON.stringify(meta)));
-                    const fileName = `./template/${templates[meta.templateIndex]}`;
-                    const extensionFileName = fileName.split('.').reverse()[0];
-                    const content = fs.readFileSync(fileName).toString();
-                    // const result = handlebars.compile(content)(meta);
+
+                    // record文件
+                    const recordFileName = `./record/${records[meta.recordIndex]}`;
+                    const recordExtensionFileName = recordFileName.split('.').reverse()[0];
+                    const recordContent = fs.readFileSync(recordFileName).toString();
+                    // const result = handlebars.compile(recordContent)(meta);
+
+                    // speech文件
+                    roleAbbrNameKVname = {
+                        xz:'先知',
+                        nw:'女巫',
+                    }
+                    let role = speechs[meta.roleIndex] ? speechs[meta.roleIndex] : roleAbbrNameKVname[meta.roleIndex.toLowerCase()]
+                    const speechFileName = `./speech/${role}.txt`;
+                    const speechContent = fs.readFileSync(speechFileName).toString();
+
+                    // 时间
                     let timeName = new Date().format('yyyy-MM-dd HH时mm分');
-                    const result = `${timeName}\n${content}`;
-                    fs.writeFileSync(`./wolf/${timeName}-${meta.num}人局.${extensionFileName}`, result);
+
+                    // 开始写入结果
+                    const result = [
+                        timeName,
+                        recordContent,
+                        (recordExtensionFileName == 'md' ? '## 发言模板' : '【发言模板】'),
+                        speechContent,
+                    ].join('\n');
+                    fs.writeFileSync(`./wolf/${timeName}-${meta.num}人局.${recordExtensionFileName}`, result);
                     // program.command(`cd project/zt/${year}/${name}`); // 这句代码尝试了一下无效。。
                     spinner.succeed();
                     console.log(symbols.success, chalk.green('项目初始化成功'));
